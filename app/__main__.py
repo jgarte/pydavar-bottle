9e118c7c63bcd1c309052a6e29c13cb13a306f96
Martin Grünwald <martingrunwald91@gmail.com>
2021-08-17 19:32:05 +0200

    somehow doing things

--- app/__main__.py
+++ app/__main__.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
-from bottle import route, run, template, static_file, get
+from bottle import route, request, run, template, static_file, get
 import os
 from settings import BASE_DIR
-
+from search import search
 
 @get('/static/<filepath>')
 def g_static(filepath):
     return static_file(filepath, root=os.path.join(BASE_DIR, "statics"))
 
-@route('/index')
+@route('/')
 def r_index():
-    pass
+    return template(os.path.join(BASE_DIR, "templates/t_index.html"))
 
-@route('/search/<string>')
-def r_search(string):
-    pass
+@route('/search')
+def r_search():
+    r = request.query.decode()
+    print(r)
+    string = r['string']
+    dict_file = "dicts/cz_he.txt.json"
+    result = search(dict_file, string)
+    return result
 
 # main() runs run :D
 def main(*args):
--- app/search.py
+++ app/search.py
@@ -4,14 +4,15 @@ from settings import BASE_DIR
 
 print(BASE_DIR)
 
-def search(string):
+def search(dict_file, string):
     
-    dict_src = 'dicts/cz_he.json' 
+    dict_path = os.path.join(BASE_DIR, dict_file)
+    result = {}
 
-    with open(dict_src, 'r') as file:
+    with open(dict_path, 'r') as file:
         dicty = json.load(file)
         for key, value in dicty.items():
             if key.startswith(string):
                 item = {key: value}
                 result.update(item)
-                return result
+    return result
